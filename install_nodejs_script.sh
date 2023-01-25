22c44de20841e8b64cbe5f900a550826eef1d48d
Haseeb <haseeb@ubuntu-sb23.fedora>
2023-01-25 19:04:35 +0530

    Updated the nodejs script to install gh

--- install_nodejs_script.sh
+++ install_nodejs_script.sh
@@ -8,7 +8,9 @@ curl -sL https://deb.nodesource.com/setup_18.x -o ~/Downloads/nodesource_setup.s
 sudo bash ~/Downloads/nodesource_setup.sh
 sudo apt install nodejs -y
 sudo npm update -g
+
 sudo apt install git -y
+sudo apt install gh -y
 
 # Install VS Code (Note: .deb file should exist in the directory)
 #sudo apt install ./code_1.74.3-1673284829_amd64.deb -y
