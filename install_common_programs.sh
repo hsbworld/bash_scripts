ddbfdaaca30b8ce9902b799f00e820eec20cc835
Haseeb <haseeb@ubuntu-sb28.fedora>
2023-01-27 15:07:36 +0530

    Updated echo statements to reflect the current activity being performed

--- install_common_programs.sh
+++ install_common_programs.sh
@@ -1,5 +1,13 @@
  #!/bin/bash
 
+echo "Installing git"
 sudo apt install git -y
+echo "Done"
+
+echo "Installing gh"
 sudo apt install gh -y
+echo "Done"
+
+echo "Installing nano"
 sudo apt install nano -y
+echo "Done"
--- install_nodejs_script.sh
+++ install_nodejs_script.sh
@@ -8,11 +8,19 @@ cd ..
 mkdir -p ~/Downloads
 curl -sL https://deb.nodesource.com/setup_18.x -o ~/Downloads/nodesource_setup.sh
 sudo bash ~/Downloads/nodesource_setup.sh
-sudo apt install nodejs -y
-sudo npm update -g
 
+echo "Installing Node JS"
+sudo apt install nodejs -y
+echo "Done"
+
+echo "Updating npm"
+sudo npm update -g
+echo "Done"
+
+echo "Installing git and github"
 sudo apt install git -y
 sudo apt install gh -y
+echo "Done"
 
 # Install VS Code (Note: .deb file should exist in the directory)
 echo "Installing VS Code"
@@ -20,4 +28,6 @@ sudo apt install ./code_1.74.3-1673284829_amd64.deb -y
 echo "Done"
 
 # Install angular
+echo "Installing Angular"
 sudo npm install -g @angular/cli
+echo "Done"
